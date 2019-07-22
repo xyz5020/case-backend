5e631151b8ff0490588bed1a798d3110dc88f43a
xyz5020 <xyzmail520@gmail.com>
2019-07-22 17:46:33 +0800

    update

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+const indexRouter = require('./router/index')
+
+mongoose.connect('mongodb://localhost/case', {useNewUrlParser: true});
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+
+let entries = []
+app.locals.entries = entries
+
+app.use(express.json())
+app.use((req, res, next) => {
+  console.log("In comes a " + req.method + " to " + req.url)
+  next()
+})
+
+app.use('/', indexRouter)
+
+// 404
+app.use((req, res) => {
+  res.status(404).send("404! Sorry can't find that!")
+})
+
+// error handling
+app.use((err, req, res) => {
+  console.log(err.stack)
+  res.status(500).send("500! Server error")
+})
+
+app.listen(3000, () => { console.log('case-backend server starting') })
