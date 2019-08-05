d6df41ffcf94c5843e6bb143fe8924df7291332a
xyz5020 <xyzmail520@gmail.com>
2019-08-05 17:26:14 +0800

    update

---
+++ spider/case_spider.py
@@ -0,0 +1,14 @@
+import scrapy
+
+
+class CaseSpider(scrapy.Spider):
+    name = "case_spider"
+    start_urls = ['https://ras.papercept.net/conferences/conferences/CASE19/program/CASE19_ContentListWeb_1.html']
+
+    def parse(self, response):
+        SET_SELECTOR: ".set"
+        for item in response.css(SET_SELECTOR):
+            name_selector = 'h1 ::text'
+            yield {
+                'name': item.css(name_selector).extract_first(),
+            }
